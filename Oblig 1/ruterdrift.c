d1aa1a321395f5d2509a1f5ece5c4ae2dd540f92
Patrick Kvistbråten Gove <patrikg@student.matnat.uio.no>
2019-02-20 17:40:10 +0100

    Start
    start på oblig1

--- Oblig 1/ruterdrift.c
+++ Oblig 1/ruterdrift.c
@@ -1,1 +1,18 @@
 //ruterdrift.c
+//
+
+struct ruter{
+    unsigned char ruterID;
+    unsigned char FLAGG;
+    unsigned char length;
+    char[] modell;
+}
+
+int main(){
+    unsigned char buffer[255];
+    FILE *ptr;
+
+    ptr = fopen("test.bin","rb");  // r for read, b for binary
+
+    fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
+}
