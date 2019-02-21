f79bf1e1666e990bc974622159bc4d0c082a48ee
Patrick Kvistbråten Gove <patrikg@student.matnat.uio.no>
2019-02-21 14:07:28 +0100

    Update ruterdrift.c

--- Oblig 1/ruterdrift.c
+++ Oblig 1/ruterdrift.c
@@ -1,18 +1,45 @@
 //ruterdrift.c
 //
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct ruter{
     unsigned char ruterID;
     unsigned char FLAGG;
     unsigned char length;
-    char[] modell;
-}
+    char modell[];
+};
 
-int main(){
-    unsigned char buffer[255];
-    FILE *ptr;
+int main(int argc, char **argv){
+  FILE* file;
+  char buffer[255];
+  int N;
 
-    ptr = fopen("test.bin","rb");  // r for read, b for binary
+  if(argc != 2){
+    printf("Legg til fil navn som argument\n");
+    exit(EXIT_FAILURE);
+  }
 
-    fread(buffer,sizeof(buffer),1,ptr); // read 10 bytes to our buffer
+
+  file = fopen(argv[1], "rb");
+  if (file == NULL) {
+    printf("Fant ikke fil\n");
+    exit(EXIT_FAILURE);
+  }
+
+  fgets(buffer, 255, file);
+  N = buffer[0];
+  printf("%d\n",N);
+
+
+  while (fgets(buffer, 255, file)) {
+    int len = strlen(buffer);
+    for (int i = 1; i < len; i++) {
+      printf("ID: %d", buffer[0]);
+    }
+    printf("\n");
+  }
+
+  fclose(file);
 }
